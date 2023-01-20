7806119dab6b40cc746d57459e6961fabab58c7c
musa muhammed <muhammed_m@qwasar.io>
2023-01-20 14:34:48 +0000

    display user input stage 1

--- src/component/ReactCalculator.js
+++ src/component/ReactCalculator.js
@@ -1,36 +1,53 @@
 import "./Calculator.css"
+import { useState } from "react"
 
 const ReactCalculator = function(){
+    const [currentValue, setCurrentValue] = useState(0)
+    const handleClick=(event)=>{
+        let buttonClass = event.target.className
+        let buttonValue = event.target.innerHTML
+        switch (buttonClass) {
+            case "numbers":
+                setCurrentValue(currentValue.toString().concat(buttonValue.toString()))
+                break;
+            case "operator":
+                alert("its an operator")
+                break;
+            default:
+                alert("its a special one")
+                break;
+        }
+    }
     return(
         <div className="mainContainer">
             <div className="container">
                 <div className="displayScreen">
-                    <h1>7 x 7 = 49</h1>
+                    <h1>{currentValue}</h1>
                 </div>
                 <div className="buttons">
                     <div className="eachButton">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>+</button>
+                        <button className="numbers" onClick={handleClick}>1</button>
+                        <button className="numbers" onClick={handleClick}>2</button>
+                        <button className="numbers" onClick={handleClick}>3</button>
+                        <button className="operator" onClick={handleClick}>+</button>
                     </div>
                     <div className="eachButton">
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button>-</button>
+                        <button className="numbers" onClick={handleClick}>4</button>
+                        <button className="numbers" onClick={handleClick}>5</button>
+                        <button className="numbers" onClick={handleClick}>6</button>
+                        <button className="operator" onClick={handleClick}>-</button>
                     </div>
                     <div className="eachButton">
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button>*</button>
+                        <button className="numbers" onClick={handleClick}>7</button>
+                        <button className="numbers" onClick={handleClick}>8</button>
+                        <button className="numbers" onClick={handleClick}>9</button>
+                        <button className="operator" onClick={handleClick}>*</button>
                     </div>
                     <div className="eachButton">
-                        <button className="special">C</button>
-                        <button>0</button>
-                        <button className="special">=</button>
-                        <button>/</button>
+                        <button className="special" onClick={handleClick}>C</button>
+                        <button className="numbers" onClick={handleClick}>0</button>
+                        <button className="special" onClick={handleClick}>=</button>
+                        <button className="operator" onClick={handleClick}>/</button>
                     </div>
                 </div>
             </div>
